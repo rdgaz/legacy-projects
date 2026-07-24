0e711da1e4fc317565bf68f65af0268fe5156ea2
Rodrigo A. Souza <rodrigo.a.souzasd@gmail.com>
2026-07-23 23:29:28 -0300

    Add files via upload

---
+++ InfoComForm/InfoComForm/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace InfoComForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.CbSetor = new System.Windows.Forms.ComboBox();
+            this.TbNome = new System.Windows.Forms.TextBox();
+            this.TbEmail = new System.Windows.Forms.TextBox();
+            this.TbRamal = new System.Windows.Forms.TextBox();
+            this.BtnOk = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CbSetor
+            // 
+            this.CbSetor.FormattingEnabled = true;
+            this.CbSetor.Items.AddRange(new object[] {
+            "Área Técnica",
+            "Comercial",
+            "Desenvolvimento",
+            "Financeiro",
+            "Qualidade",
+            "RH \\ Recpção",
+            "HelpDesk",
+            "Treinamento"});
+            this.CbSetor.Location = new System.Drawing.Point(18, 127);
+            this.CbSetor.Name = "CbSetor";
+            this.CbSetor.Size = new System.Drawing.Size(136, 21);
+            this.CbSetor.TabIndex = 0;
+            this.CbSetor.Text = "Nenhum";
+            this.CbSetor.SelectedIndexChanged += new System.EventHandler(this.CbSetor_SelectedIndexChanged);
+            // 
+            // TbNome
+            // 
+            this.TbNome.Location = new System.Drawing.Point(18, 25);
+            this.TbNome.MaxLength = 20;
+            this.TbNome.Name = "TbNome";
+            this.TbNome.Size = new System.Drawing.Size(179, 20);
+            this.TbNome.TabIndex = 1;
+            // 
+            // TbEmail
+            // 
+            this.TbEmail.Location = new System.Drawing.Point(17, 76);
+            this.TbEmail.MaxLength = 35;
+            this.TbEmail.Name = "TbEmail";
+            this.TbEmail.Size = new System.Drawing.Size(180, 20);
+            this.TbEmail.TabIndex = 2;
+            this.TbEmail.TextChanged += new System.EventHandler(this.TbEmail_TextChanged);
+            // 
+            // TbRamal
+            // 
+            this.TbRamal.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.TbRamal.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TbRamal.Location = new System.Drawing.Point(18, 181);
+            this.TbRamal.MaxLength = 4;
+            this.TbRamal.Multiline = true;
+            this.TbRamal.Name = "TbRamal";
+            this.TbRamal.Size = new System.Drawing.Size(69, 32);
+            this.TbRamal.TabIndex = 3;
+            this.TbRamal.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.TbRamal.TextChanged += new System.EventHandler(this.TbRamal_TextChanged);
+            this.TbRamal.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TbRamal_KeyPress);
+            // 
+            // BtnOk
+            // 
+            this.BtnOk.Location = new System.Drawing.Point(103, 181);
+            this.BtnOk.Name = "BtnOk";
+            this.BtnOk.Size = new System.Drawing.Size(94, 32);
+            this.BtnOk.TabIndex = 4;
+            this.BtnOk.Text = "Ok";
+            this.BtnOk.UseVisualStyleBackColor = true;
+            this.BtnOk.Click += new System.EventHandler(this.BtnOk_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Email Completo:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Setor:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Ramal:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(226, 230);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnOk);
+            this.Controls.Add(this.TbRamal);
+            this.Controls.Add(this.TbEmail);
+            this.Controls.Add(this.TbNome);
+            this.Controls.Add(this.CbSetor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Info Pc";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox CbSetor;
+        private System.Windows.Forms.TextBox TbNome;
+        private System.Windows.Forms.TextBox TbEmail;
+        private System.Windows.Forms.TextBox TbRamal;
+        private System.Windows.Forms.Button BtnOk;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ InfoComForm/InfoComForm/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Management;
+using System.Net;
+namespace InfoComForm
+{
+    public partial class Form1 : Form
+    {
+        string endereco = "";
+        string proc;
+        string win;
+        bool ev = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetIP();
+            try
+            {
+                win = System.Environment.GetEnvironmentVariable("TMP");
+            }
+            catch (Exception)
+            {
+                win = System.Environment.GetEnvironmentVariable("APPDATA");
+                throw;
+            }
+            ManagementObjectSearcher s2 = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_Processor");
+
+            foreach (var obj in s2.Get())
+            {
+                proc += obj["Name"].ToString();
+            }
+
+        }
+
+        private void BtnOk_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TbRamal_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TbRamal_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = (!char.IsDigit(e.KeyChar)) && (!char.IsControl(e.KeyChar));
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void GetIP()
+        {
+            string strHostName = System.Net.Dns.GetHostName();
+            IPHostEntry ipEntry = System.Net.Dns.GetHostEntry(strHostName);
+            IPAddress[] addr = ipEntry.AddressList;
+
+            endereco = addr[addr.Length - 1].ToString();
+
+            if (!endereco.Contains(".200."))
+            {
+                if(!endereco.Contains(".100."))
+                {
+                    endereco = addr[addr.Length - 2].ToString(); 
+                }
+            }
+            if (!endereco.Contains(".200."))
+            {
+                if (!endereco.Contains(".100."))
+                {
+                    endereco = addr[addr.Length - 3].ToString();
+                }
+            }
+            if (!endereco.Contains(".200."))
+            {
+                if (!endereco.Contains(".100."))
+                {
+                    endereco = addr[addr.Length - 4].ToString();
+                }
+            }
+        }
+        private void GeraBat(string caminho, string nomearq)
+        {
+
+            string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            String nome = TbNome.Text, mail = TbEmail.Text, setor = CbSetor.Text, ramal = TbRamal.Text;
+            int CodSetor = CbSetor.SelectedIndex;
+            {
+                using (System.IO.StreamWriter sw = new System.IO.StreamWriter(@caminho + "\\" + nomearq, false, System.Text.Encoding.UTF8))
+                {
+                    sw.WriteLine(":: gera td e logs");
+                    sw.WriteLine("cls");
+                    sw.WriteLine("@echo off");
+                    sw.WriteLine("color 2");
+                    sw.WriteLine("net use I:  /delete /yes");
+                    sw.WriteLine(":: versão 3.0.1");
+                    sw.WriteLine("setlocal");
+                    sw.WriteLine("set /a \"Tipo_pc=0\" ");
+                    sw.WriteLine("set \"command=2^>nul WMIC SystemEnclosure Get ChassisTypes /value\" ");
+                    sw.WriteLine("for /f \"tokens=2 delims=={}\" %%A IN ('%command%') do ( ");
+                    sw.WriteLine("2>nul set /a \"Tipo_pc=%%A\" ");
+                    sw.WriteLine(")");
+                    sw.WriteLine("if %Tipo_pc%==0 (set /a \"Tipo_pc=2\") ");
+                    sw.WriteLine("set _=Outro;Desconhecido;Desktop;Desktop de baixo perfil");
+                    sw.WriteLine("set _=%_%;Pizza Box;Mini Torre;Torre;Notebook;Laptop;Notebook");
+                    sw.WriteLine("set _=%_%;Palm top;Estacao Documento;Todos em um;Sub Notebook");
+                    sw.WriteLine("set _=%_%;Economia de espaco;Vmware;Sistema principal;Expansivo");
+                    sw.WriteLine("set _=%_%;Subexpansao;Bus de sistema ;Periferico");
+                    sw.WriteLine("set _=%_%;Backup;Rack de sistema;PC selado");
+                    sw.WriteLine("for /f \"tokens=%Tipo_pc% delims=; eol=\" %%A in (\"%_%\") do ( ");
+                    sw.WriteLine("set tipo=%Tipo_pc% : %%A");
+                    sw.WriteLine(")");
+                    sw.WriteLine("");
+                    sw.WriteLine("title Info.exe - Created By Rodrigo A. Souza");
+                    sw.WriteLine("net use I: \\\\192.168.0.5\\arquivos /yes");
+                    sw.WriteLine("cls");
+                    sw.WriteLine("set s=systeminfo");
+                    sw.WriteLine("set pc=%COMPUTERNAME%");
+                    sw.WriteLine("set us=%homepath%");
+                    sw.WriteLine("cls");
+                    sw.WriteLine("echo Gerando arquivos, aguarde...");
+                    sw.WriteLine("(");
+                    sw.WriteLine("time /t");
+                    sw.WriteLine("date /t");
+                    sw.WriteLine("echo Nome:  " + nome + "");
+                    sw.WriteLine("echo Email: " + mail + "");
+                    sw.WriteLine("echo Setor: " + setor + "");
+                    sw.WriteLine("echo Ramal: " + ramal + "");
+                    sw.WriteLine("echo User: " + userName + " - %us%");
+                    sw.WriteLine("echo Tipo: %Tipo%");
+                    sw.WriteLine("echo Processador: \"" + proc + "\"");
+                    sw.WriteLine("ipconfig | find \"IPv4\" ");
+                    sw.WriteLine("%s%");
+                    sw.WriteLine("tasklist");
+                    sw.WriteLine(")>" + win + "\\" + CodSetor + "_%pc%.log");
+                    sw.WriteLine("copy /y %tmp%\\" + CodSetor + "_%pc%.log S:\\Publico\\T.I\\inf\\logs\\" + CodSetor + "_%pc%.log");
+                    sw.WriteLine("");
+                    sw.WriteLine("(");
+                    sw.WriteLine("echo        ^<tr^>");
+                    sw.WriteLine("echo          ^<td^>" + nome + "^</td^>");
+                    sw.WriteLine("echo   ^<td^> ^<a href=\"mailto:" + nome + " - " + setor + "<" + mail + ">\"^> " + mail + " ^</a^> ^</td^>");
+                    sw.WriteLine("echo          ^<td^> %us% ^</td^>");
+                    sw.WriteLine("echo          ^<td^>%tipo%^</td^>");
+                    sw.WriteLine("echo ^<td^> \"" + proc + "\"^</td^>");
+                    sw.WriteLine("echo ^<td^>" + endereco + "^</td^>");
+                    sw.WriteLine("echo		  ^<td^> ^<a href=\"file:///S://Publico/T.I/inf/logs/" + CodSetor + "_%pc%.log\" target=\"_blank\"^>LOG^</a^> ^</td^> ");
+                    sw.WriteLine("echo        ^</tr^>");
+                    sw.WriteLine(")>" + win + "\\" + CodSetor + "_%pc%.txt ");
+                    sw.WriteLine("copy /y %tmp%\\" + CodSetor + "_%pc%.txt S:\\Publico\\T.I\\inf\\tr\\" + CodSetor + "_%pc%.txt ");
+                    sw.WriteLine("");
+                    sw.WriteLine("echo msgbox \"Arquivos enviados com sucesso!\">\"%tmp%\\t.vbs\" ");
+                    sw.WriteLine("cscript/nologo \"%tmp%\\t.vbs\" ");
+                    sw.WriteLine("net use I:  /delete /yes");
+
+                    sw.Close();
+                }
+
+            }
+
+        }
+        private bool camposValidos()
+        {
+            if (TbNome.Text.Length > 0)
+            {
+                if (CbSetor.Text != "Nenhum")
+                {
+                    if (ev == true)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        private void BtnOk_Click_1(object sender, EventArgs e)
+        {
+
+            if (camposValidos())
+            {
+                GeraBat(win, "info_li.bat");
+                System.Diagnostics.Process.Start(@"" + win + "\\info_li.bat");
+                Environment.Exit(0);
+            }
+            else
+            {
+                MessageBox.Show("Preencha todos os campos solicitados!", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+        private void TbEmail_TextChanged(object sender, EventArgs e)
+        {
+
+            if (TbEmail.Text.Contains("@"))
+            {
+                if (TbEmail.Text.Contains(".net"))
+                {
+                    ev = true;
+                    label2.ForeColor = System.Drawing.Color.Green;
+                }
+            }
+            else
+            {
+                ev = false;
+                label2.ForeColor = System.Drawing.Color.Red;
+
+            }
+        }
+
+
+        private void tbcargo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CbSetor_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
